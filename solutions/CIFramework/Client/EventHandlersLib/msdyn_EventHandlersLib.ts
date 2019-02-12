a90074749897a2d9a2b6f9a9ceec0186d010df31
pavankumar kolla <avk@microsoft.com>
2019-02-12 18:02:57 +0530

    changing error message and adding try catch

--- solutions/CIFramework/Client/EventHandlersLib/msdyn_EventHandlersLib.ts
+++ solutions/CIFramework/Client/EventHandlersLib/msdyn_EventHandlersLib.ts
@@ -4,22 +4,36 @@ module CIFramework {
 	let urlRegExp = new RegExp('^((www\.)?[0-9a-zA-Z-_\.]+[a-zA-Z]{2,}$)')
 	
 	export function wildcardCheckOnLandingUrl() {
-		let landingURL: URL = new URL(Xrm.Page.getAttribute("msdyn_landingurl").getValue());
-		if (!urlRegExp.test(landingURL.hostname)) {
-			let alertMessage: string = "The following characters are not allowed in the Channel URL: \n !~@{}$/\\%^()&*[]+=|,\"<>\':?; \n\n\n Allowed set of characters are: \n Uppercase letter (A-Z), Lowercase letter (a-z), Number (0-9), Hyphen (-), Period(.) and Underscore (_).";
-			let buttonText: string = "OK";
-			let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
-			Xrm.Navigation.openAlertDialog(alertStrings);
+		try
+		{
+			let landingURL: URL = new URL(Xrm.Page.getAttribute("msdyn_landingurl").getValue());
+			if (!urlRegExp.test(landingURL.hostname)) {
+				let alertMessage: string = "Enter a valid URL";
+				let buttonText: string = "OK";
+				let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
+				Xrm.Navigation.openAlertDialog(alertStrings);
+			}
+		}
+		catch (e)
+		{
+			console.log(e);
 		}
 	}
 
 	export function wildcardCheckOnTrustedDomain() {
+		try
+		{
 		let trustedDomain: URL = new URL(Xrm.Page.getAttribute("msdyn_trusteddomain").getValue());
 		if (!urlRegExp.test(trustedDomain.hostname)) {
-			let alertMessage: string = "The following characters are not allowed in the Trusted Domain: \n !~@{}$/\\%^()&*[]+=|,\"<>\':?; \n\n\n Allowed set of characters are: \n Uppercase letter (A-Z), Lowercase letter (a-z), Number (0-9), Hyphen (-), Period(.) and Underscore (_).";
+			let alertMessage: string = "Enter a valid Trusted Domain URL";
 			let buttonText: string = "OK";
 			let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
 			Xrm.Navigation.openAlertDialog(alertStrings);
+			}
+		}
+		catch (e)
+		{
+			console.log(e);
 		}
 	}
 }
