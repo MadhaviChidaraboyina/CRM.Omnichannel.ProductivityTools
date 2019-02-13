817db7f31f7fe30346013d292cf15358d94d59a3
pavankumar kolla <avk@microsoft.com>
2019-02-13 16:43:43 +0530

    indentation change'

--- solutions/CIFramework/Client/EventHandlersLib/msdyn_EventHandlersLib.ts
+++ solutions/CIFramework/Client/EventHandlersLib/msdyn_EventHandlersLib.ts
@@ -7,13 +7,13 @@ module CIFramework {
 	export function wildcardCheckOnLandingUrl() {
 		try
 		{
-			let landingURL: URL = new URL(Xrm.Page.getAttribute("msdyn_landingurl").getValue());
-			if (!urlRegExp.test(landingURL.hostname) || !protocolRegExp.test(landingURL.protocol)) {
-				let alertMessage: string = "Enter a valid URL";
-				let buttonText: string = "OK";
-				let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
-				Xrm.Navigation.openAlertDialog(alertStrings);
-			}
+		let landingURL: URL = new URL(Xrm.Page.getAttribute("msdyn_landingurl").getValue());
+		if (!urlRegExp.test(landingURL.hostname) || !protocolRegExp.test(landingURL.protocol)) {
+			let alertMessage: string = "Enter a valid URL";
+			let buttonText: string = "OK";
+			let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
+			Xrm.Navigation.openAlertDialog(alertStrings);
+		}
 		}
 		catch (e)
 		{
@@ -25,12 +25,12 @@ module CIFramework {
 		try
 		{
 		let trustedDomain: URL = new URL(Xrm.Page.getAttribute("msdyn_trusteddomain").getValue());
-			if (!urlRegExp.test(trustedDomain.hostname) || !protocolRegExp.test(trustedDomain.protocol)) {
+		if (!urlRegExp.test(trustedDomain.hostname) || !protocolRegExp.test(trustedDomain.protocol)) {
 			let alertMessage: string = "Enter a valid Trusted Domain URL";
 			let buttonText: string = "OK";
 			let alertStrings: any = { confirmButtonLabel: buttonText, text: alertMessage };
 			Xrm.Navigation.openAlertDialog(alertStrings);
-			}
+		}
 		}
 		catch (e)
 		{
