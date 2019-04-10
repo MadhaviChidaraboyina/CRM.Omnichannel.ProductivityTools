dbcf332550d47df5ec90c151dfbacbfdbe707ca2
Sourabh Verma <soverm@microsoft.com>
2019-04-10 13:24:44 +0000

    Merged PR 303634: Add unknown icon for presence.
    1. Add unknown icon for presence.
    2. Also disabled open presence dialog from ribbon until the presence is set. Same as in OCW
    
    https://aurora80393.aurora80393dom.extest.microsoft.com/CITTest/main.aspx?appid=c8d0b980-ce59-e911-8194-000d3a6cf5ff
    
    Related work items: #1448686

--- solutions/CIFramework/Client/OpenDialogLib/OpenPresenceDialogControl.ts
+++ solutions/CIFramework/Client/OpenDialogLib/OpenPresenceDialogControl.ts
@@ -12,26 +12,32 @@ namespace Microsoft.CIFramework.Internal {
 
 	export class OpenPresenceDialogControl {
 
-        private static instance: OpenPresenceDialogControl;
+		private static instance: OpenPresenceDialogControl;
 
-        // Empty Constructor
-        constructor() { }
-        
-        public static get Instance(): OpenPresenceDialogControl {
+		// Empty Constructor
+		constructor() { }
+		
+		public static get Instance(): OpenPresenceDialogControl {
 			if (this.instance == null) {
 				this.instance = new OpenPresenceDialogControl();
 			}
 			return this.instance;
 		}
 
-        public openPresenceDialog(e: any): any {
-            const that = this;
-            const dialogParams: XrmClientApi.DialogParameters = {};
-            dialogParams[Constants.LAST_BUTTON_CLICKED] = "";
-            const dialogOptions: XrmClientApi.DialogOptions = { width: 300, height: 300, position: XrmClientApi.Constants.WindowPosition.center };
-            Xrm.Navigation.openDialog(Constants.SET_PRESENCE_MDD, dialogOptions, dialogParams).then(function (dialogParams: any) {
-            });
-        }
+		public openPresenceDialog(e: any): void {
+			let presenceButton = (<HTMLButtonElement>window.top.document.querySelector(Constants.PRESENCE_BUTTON_DATA_ID));
+			let presence_img = presenceButton.getElementsByTagName("img");
+			if (presence_img[0].src.indexOf("/WebResources/msdyn_UnknownStatus.svg") != -1) {
+				return;
+			}
+
+			const that = this;
+			const dialogParams: XrmClientApi.DialogParameters = {};
+			dialogParams[Constants.LAST_BUTTON_CLICKED] = "";
+			const dialogOptions: XrmClientApi.DialogOptions = { width: 300, height: 300, position: XrmClientApi.Constants.WindowPosition.center };
+			Xrm.Navigation.openDialog(Constants.SET_PRESENCE_MDD, dialogOptions, dialogParams).then(function (dialogParams: any) {
+			});
+		}
 
 		public addPresenceCommand(e: any): any {
 			var appName: any = "";
@@ -48,5 +54,5 @@ namespace Microsoft.CIFramework.Internal {
 					});
 			});
 		}
-    }
+	}
 }
