bfc6e2aff945838842e25ae2f3a78db80b09e89c
Shobhan Garg <shogarg@microsoft.com>
2019-08-29 05:00:39 +0000

    Merged PR 355885: Macros Solution
    Adding a new solution for Macros with minify, TS Localization and RESX Localization Infra
    
    Install of Managed and Unmanaged Solutions and PDPackage is working fine
    
    Related work items: #1554883

---
+++ solutions/Macros/Client/MacrosUtilities.ts
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Macros.Utility {
+	var webresourceName = "Localization/Macros_webresource_strings.1033.resx";
+
+	export function getResourceString(key: any) {
+		var value = key;
+		if (Xrm && Xrm.Utility && Xrm.Utility.getResourceString) {
+			value = Xrm.Utility.getResourceString(webresourceName, key);
+
+			if (value === undefined || value === null) {
+				value = key;
+			}
+		}
+		return value;
+	}
+}
