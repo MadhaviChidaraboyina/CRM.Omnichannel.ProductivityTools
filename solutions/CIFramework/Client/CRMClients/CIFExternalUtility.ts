a1fe18e907b45475c166f73dd8bb6b76f8cb6afb
Danish Syed <syali@microsoft.com>
2019-09-03 09:15:36 +0000

    Merged PR 360226: Changes to expose getSessionInfoObjectAPIs in CIFramework
    Changes to expose getSessionInfoObjectAPIs in CIFramework
    
    Related work items: #1566484

---
+++ solutions/CIFramework/Client/CRMClients/CIFExternalUtility.ts
@@ -0,0 +1,26 @@
+﻿/**
+ * @license Copyright (c) Microsoft Corporation. All rights reserved.
+ */
+/// <reference path="../../../../references/external/TypeDefinitions/lib.es6.d.ts" />
+/// <reference path="../../../../packages/Crm.ClientApiTypings.1.3.2084/clientapi/XrmClientApi.d.ts" />
+/// <reference path="SessionManager.ts" />
+/// <reference path="ConsoleAppSessionManager.ts" />
+
+import Internal = Microsoft.CIFramework.Internal;
+namespace Microsoft.CIFramework.External {
+
+	export interface CIFExternalUtility {
+		getSessionInfoObject(sessionId: string): Internal.SessionInfo;
+		getCurrentSessionInfoObject(): Internal.SessionInfo;
+	}
+
+	export class CIFExternalUtilityImpl extends Internal.ConsoleAppSessionManager implements CIFExternalUtility {
+		public getSessionInfoObject(sessionId: string): Internal.SessionInfo {
+			return this.sessions.get(sessionId);
+		}
+
+		public getCurrentSessionInfoObject(): Internal.SessionInfo {
+			return this.sessions.get(this.getFocusedSession());
+		}
+	}
+}
