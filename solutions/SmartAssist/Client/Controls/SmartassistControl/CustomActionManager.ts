3cc398c12752fd7238c0d13e8bbba21a2cd7400b
HariPriya Thangavel <hathanga@microsoft.com>
2020-03-31 19:26:54 +0530

    OOB Custom actions for Similar Case

--- solutions/SmartAssist/Client/Controls/SmartassistControl/CustomActionManager.ts
+++ solutions/SmartAssist/Client/Controls/SmartassistControl/CustomActionManager.ts
@@ -9,14 +9,25 @@ module MscrmControls.ProductivityPanel.Smartassist {
 	export class CustomActionManager {
 
 		public Name: string = "";
+		/*
+		 * Parameters will contain
+		 * kbLink - KBSearch
+		 * recordId - RecordId for CustomAction OpenForm/OpenCase
+		 * entityName - Logical Name of the entity for CustomAction OpenForm
+		 **/
 		public Parameters: any = {};
 		public isValidAction: boolean = false;
+		private logger: TelemetryLogger;
 
 		/* 
 		 * Constructor
 		 * @params ActionData Action data to be set to invoke Custom action on Action.Submit
 		 */
 		public constructor(ActionData: any) {
+
+			// Initialize Telemetry Repoter
+			this.logger = new Smartassist.TelemetryLogger(SmartassistControl._context);
+
 			if (!SmartassistControl._context.utils.isNullOrUndefined(ActionData)) {
 				if (ActionData.CustomAction) {
 					this.Name = ActionData.CustomAction;
@@ -30,6 +41,10 @@ module MscrmControls.ProductivityPanel.Smartassist {
 
 		/* 
 		 * Validate Custom Action Data
+		 * Validate whethere Parameters contains the following data
+		 * kbLink - KBSearch
+		 * recordId - RecordId for CustomAction OpenForm/OpenCase
+		 * entityName - Logical Name of the entity for CustomAction OpenForm
 		 **/
 		private ValidateCustomAction(): void {
 			switch (this.Name) {
@@ -39,6 +54,29 @@ module MscrmControls.ProductivityPanel.Smartassist {
 					if (this.Parameters && this.Parameters.kbLink) {
 						this.isValidAction = true;
 					}
+					break;
+				case CustomActionConstants.OpenForm:
+					// Verify whether entityName and recordId are present in Custom action parameters
+					if (this.Parameters && this.Parameters.entityId && this.Parameters.entityName) {
+						this.isValidAction = true;
+					}
+					break;
+				case CustomActionConstants.OpenCase:
+					// Verify whether recordId is present in Custom action parameters
+					this.Parameters.entityName = CustomActionConstants.EntityCaseName;
+					if (this.Parameters && this.Parameters.entityId) {
+						this.isValidAction = true;
+					}
+					break;
+				case CustomActionConstants.CloneEntity:
+					// Verify whether entityName is present in Custom action parameters
+					if (this.Parameters && this.Parameters.entityName) {
+						this.isValidAction = true;
+					}
+					break;
+				case CustomActionConstants.CloneCase:
+					this.Parameters.entityName = CustomActionConstants.EntityCaseName;
+					this.isValidAction = true;
 					break;
 				default:
 					// Custom CustomAction
@@ -65,10 +103,75 @@ module MscrmControls.ProductivityPanel.Smartassist {
 		/*
 		 * Custom Action to open KB article in a new browser tab
 		 **/
-		public CustomActionOpenKB(): void {
+		public CustomActionOpenKB(): any {
 			window.open(this.Parameters.kbLink);
-			Promise.resolve();
+			return Promise.resolve();
 		}
 
+		/*
+		 * Custom Action to open Form in App tab using CIF
+		 * @params entityLogicalName Entity Logical Name
+		 * @params recordId Record Id
+		 **/
+		public CustomActionOpenForm() {
+
+			let eventParameters = new Smartassist.EventParameters();
+			eventParameters.addParameter("Message", "Running Custom Action - OpenForm");
+			eventParameters.addParameter("Entity", this.Parameters.entityName);
+			eventParameters.addParameter("RecordId", this.Parameters.recordId);
+
+			try {
+
+				var tabInput = {
+					pageInput: {
+						pageType: CustomActionConstants.RecordPageType,
+						entityName: this.Parameters.entityName,
+						entityId: this.Parameters.entityId,
+						data: this.Parameters.data,
+					},
+					options: {
+						isFocused: true
+					}
+				};
+
+				new (window as any).top.Microsoft.CIFramework.External.CIFExternalUtilityImpl().createTab(tabInput);
+
+			} catch (Error) {
+				eventParameters.addParameter("Exception Details", Error);
+				this.logger.logError(this.logger.baseComponent, "CustomActionOpenForm", "Error occurred while running custom action - OpenForm", eventParameters);
+				return Promise.reject("Failed while executing custom action to open Form");
+			}
+		}
+
+		/*
+		 * Custom Action to create entity. This opens up create form in a new tab with the prefilled data
+		 * Parameters should passed in data
+		 **/
+		public CustomActionCreateEntity() {
+
+			let eventParameters = new Smartassist.EventParameters();
+			eventParameters.addParameter("Message", "Running Custom Action - CloneCase");
+			eventParameters.addParameter("Entity", this.Parameters.entityName);
+			eventParameters.addParameter("Data", this.Parameters.data);
+
+			try {
+				var tabInput = {
+					pageInput: {
+						pageType: CustomActionConstants.RecordPageType,
+						entityName: this.Parameters.entityName,
+						data: this.Parameters.data
+					},
+					options: {
+						isFocused: true
+					}
+				};
+				new (window as any).top.Microsoft.CIFramework.External.CIFExternalUtilityImpl().createTab(tabInput);
+
+			} catch (Error) {
+				eventParameters.addParameter("Exception Details", Error);
+				this.logger.logError(this.logger.baseComponent, "CustomActionCreateEntity", "Error occurred while running custom action - CreateEntity", eventParameters);
+				return Promise.reject("Failed while executing custom action to create entity");
+			}
+		}
 	}
 }
