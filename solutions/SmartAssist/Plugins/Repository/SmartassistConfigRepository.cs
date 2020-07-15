a4ee08becbc8400c4b181d9f2606f4bad6801e63
Hari Priya🐿 <Hari.Thangavel@microsoft.com>
2020-07-15 20:01:02 +0000

    Merged PR 469645: Plugin to Enable/Disable SA Config based on Admin settings
    Plugin to update Enable/Disable SA Config based on Admin settings
    
    **Testing Details**
    http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx#752956181
    Administrator
    Aurora_0522b47
    
    To test this:
    
    Open [Active Smart Assist configs](http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx?appid=d362d95d-a2c6-ea11-a81b-000d3a477082&pagetype=entitylist&etn=msdyn_smartassistconfig&viewid=d2d43bd8-c80b-4d38-8767-ca3690bdf67d&viewType=1039)
    Toggle Admin settings [here ](http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx?appid=d362d95d-a2c6-ea11-a81b-000d3a477082&pagetype=entitylist&etn=msdyn_analyticsadminsettings&viewid=981cd508-5293-4ec1-9a2e-a401382fcfd0&viewType=1039)
    
    Related work items: #1927538

---
+++ solutions/SmartAssist/Plugins/Repository/SmartassistConfigRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace Microsoft.Dynamics.SmartAssist.Plugin.Repository
+{
+	using Microsoft.Dynamics.SmartAssist.Plugin.Proxies;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Xrm.Sdk;
+	using Xrm.Sdk.Query;
+
+	/// <summary>
+	/// Repository that operates on SmartassistConfig entity
+	/// </summary>
+	public class SmartassistConfigRepository : EntityRepository<msdyn_smartassistconfig>
+	{
+		private const string CSCAppmodule = "msdynce_CustomerServiceConsole";
+
+		// Filtering based on app module unique name. This will be removed once there is a way to select a specific SmartassistConfig in SAconfig
+		private const string UniqueNameKey = "msdyn_uniquename";
+		private const string UniqueCaseName = "msdyn_sa_case_suggestion_config";
+		private const string UniqueKMName = "msdyn_sa_knowledge_suggestion_config";
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SmartassistConfigRepository"/>
+		/// </summary>
+		/// <param name="organizationService">The service provides programmatic access to the metadata and data for an organization.</param>
+		public SmartassistConfigRepository(IOrganizationService organizationService)
+			: base(organizationService)
+		{
+		}
+
+		/// <summary>
+		/// Get msdyn_smartassistconfig using unique name
+		/// </summary>
+		public List<msdyn_smartassistconfig> GetSmartassistconfig()
+		{
+			var query = new QueryExpression(msdyn_smartassistconfig.EntityLogicalName)
+			{
+				Criteria =
+				{
+					FilterOperator = LogicalOperator.Or,
+					Conditions =
+					{
+						new ConditionExpression(UniqueNameKey, ConditionOperator.Equal, UniqueCaseName),
+						new ConditionExpression(UniqueNameKey, ConditionOperator.Equal, UniqueKMName),
+					}
+				},
+				ColumnSet = new ColumnSet("msdyn_suggestiontype", "msdyn_uniquename")
+			};
+			IEnumerable<msdyn_smartassistconfig> saconfig = RetrieveEntities(query);
+			return saconfig.ToList();
+		}
+	}
+}
---
+++ solutions/SmartAssist/Plugins/SmartAssistConfig/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dynamics.SmartAssist.Plugins.SmartAssistConfig
+{
+	/// <summary>
+	/// Smartassist config status reason
+	/// </summary>
+	public enum SAConfigStatusReason
+	{
+		Active = 1,
+		InActive
+	};
+
+	/// <summary>
+	/// Smartassist config's Suggestion Type Enum
+	/// </summary>
+	public enum SuggestionType
+	{
+		KBArticle = 192360000,
+		SimilarCase = 192360001
+	};
+
+	/// <summary>
+	/// Contains all the constants required for Smartassistconfig Plugins
+	/// </summary>
+	public class Constants
+	{
+		public const string UniqueCaseName = "msdyn_sa_case_suggestion_config";
+		public const string UniqueKMName = "msdyn_sa_knowledge_suggestion_config";
+		public static Guid? SuggestionSettingsRecordID = new Guid("719e8cd8-e0aa-ea11-a81c-000d3a6ce6ca");
+	}
+}
