af4e2fb40c934cd2e1ec27e7d68daa0fa0cd9a85
Hari Priya🐿 <Hari.Thangavel@microsoft.com>
2020-07-17 13:31:20 +0000

    Merged PR 470886: Revert 'Plugin to Enable/Disable SA Config based on Admin settings '
    Plugin to update Enable/Disable SA Config based on Admin settings
    
    **Testing Details**
    http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx#752956181
    Administrator
    Aurora_0522b47
    
    To test this:
    
    Open [Active Smart Assist configs](http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx?appid=d362d95d-a2c6-ea11-a81b-000d3a477082&pagetype=entitylist&etn=msdyn_smartassistconfig&viewid=d2d43bd8-c80b-4d38-8767-ca3690bdf67d&viewType=1039)
    Toggle Admin settings [here ](http://aurorav79481.aurorav79481dom.extest.microsoft.com/CITTest/main.aspx?appid=d362d95d-a2c6-ea11-a81b-000d3a477082&pagetype=entitylist&etn=msdyn_analyticsadminsettings&viewid=981cd508-5293-4ec1-9a2e-a401382fcfd0&viewType=1039)
    
    Reverts !469645
    
    Related work items: #1935202

---
+++ solutions/SmartAssist/Plugins/SmartAssistConfig/CreateAppExtensionPlugin.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Dynamics.SmartAssist.Plugins.SmartAssistConfig
+{
+	using Microsoft.Xrm.Sdk;
+	using System;
+	using System.ServiceModel;
+
+	public class CreateAppExtensionPlugin : IPlugin
+	{
+		public void Execute(IServiceProvider serviceProvider)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
