d8136f4ec01a6224c091d3f1faa895431917a358
Salini Saha <salsaha@microsoft.com>
2020-09-24 14:02:17 +0530

    narration fix

--- solutions/SmartAssist/Client/Controls/RecommendationControl/Views/ViewTemplates.ts
+++ solutions/SmartAssist/Client/Controls/RecommendationControl/Views/ViewTemplates.ts
@@ -7,7 +7,7 @@ module MscrmControls.Smartassist.Suggestion {
 	export class ViewTemplates {
 		public static CustomActionResolveIcon = '<img aria-hidden="true" alt= "icon" src={0} style="margin-right: 5px; width: 16px; height: 16px"><label>{1}</label></image>';
 		public static CardContainerTemplate = '<div id = "{0}" class="' + Constants.RecommendationCardContainerClass + '"></div>';
-		public static SuccessMessageTemplate = '<div role="alertdialog" aria-modal="true" id= {0} class="' + Constants.CustomActionSuccessMessageClass + '" style="display: flex; height: auto">{1}</div>';
-		public static FailureMessageTemplates = '<div role="alertdialog" aria-modal="true" id = {0} class="' + Constants.CustomActionFailureMessageClass + '"style="display: flex; height: auto">{1}</div>';
+		public static SuccessMessageTemplate = '<div aria-live="assertive" role="alert" id= {0} class="' + Constants.CustomActionSuccessMessageClass + '" style="display: flex; height: auto">{1}</div>';
+		public static FailureMessageTemplates = '<div aria-live="assertive" role="alert" id = {0} class="' + Constants.CustomActionFailureMessageClass + '"style="display: flex; height: auto">{1}</div>';
 	}
 }
