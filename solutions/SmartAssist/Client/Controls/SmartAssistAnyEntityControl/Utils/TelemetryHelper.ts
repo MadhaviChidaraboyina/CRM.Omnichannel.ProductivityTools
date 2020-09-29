79ac0832101c2f07f0a066d5be5b4e9fc9d8fb9b
Mahesh Panthi <v-mapant@microsoft.com>
2020-09-29 18:49:41 +0530

    Minor update in telemetry heper

--- solutions/SmartAssist/Client/Controls/SmartAssistAnyEntityControl/Utils/TelemetryHelper.ts
+++ solutions/SmartAssist/Client/Controls/SmartAssistAnyEntityControl/Utils/TelemetryHelper.ts
@@ -2,9 +2,9 @@
 
     export class TelemetryHelper {
         private recordId: string;
-		private saconfig: SAConfig;
+        private saconfig: SAConfig;
 
-		constructor(recordId: string, saConfig: SAConfig) {
+        constructor(recordId: string, saConfig: SAConfig) {
             this.recordId = recordId;
             this.saconfig = saConfig;
         }
@@ -15,76 +15,76 @@
 		 * @param error
 		 * @param additionalParameter
 		 */
-		public logTelemetryError(eventType: string, error: any, additionalParameter: Mscrm.EventParameter[]) {
-			const params = this.getTelemetryParameter(additionalParameter,)
-			SmartAssistAnyEntityControl._context.reporting.reportFailure(eventType, error, "TSG-TODO", params);
-		}
+        public logTelemetryError(eventType: string, error: any, additionalParameter: Mscrm.EventParameter[]) {
+            const params = this.getTelemetryParameter(additionalParameter,)
+            SmartAssistAnyEntityControl._context.reporting.reportFailure(eventType, error, "TSG-TODO", params);
+        }
 
 		/**
 		 * log success event.
 		 * @param eventType
 		 * @param additionalParameter
 		 */
-		public logTelemetrySuccess(eventType: string, additionalParameter: Mscrm.EventParameter[]) {
-			const params = this.getTelemetryParameter(additionalParameter);
-			SmartAssistAnyEntityControl._context.reporting.reportSuccess(eventType, params);
-		}
+        public logTelemetrySuccess(eventType: string, additionalParameter: Mscrm.EventParameter[]) {
+            const params = this.getTelemetryParameter(additionalParameter);
+            SmartAssistAnyEntityControl._context.reporting.reportSuccess(eventType, params);
+        }
 
 		/**
 		 * Get Telemetry event parameter
 		 * @param additionalParameters
 		 */
-		public getTelemetryParameter(additionalParameters: Mscrm.EventParameter[]): Mscrm.EventParameter[] {
-			var params: Mscrm.EventParameter[] = [
-				{ name: "SuggestionForEntityId", value: this.recordId },
-				{ name: "SuggestedForEntityLogicalName", value: this.saconfig.SourceEntityName },
-				{ name: "SuggestionProviderName", value: this.saconfig.SuggestionProvider },
-				{ name: "SuggestionType", value: this.saconfig.SuggestionType },
-				{ name: "IsEnabled", value: `${this.saconfig.IsEnabled}` },
-				{ name: "IsDefault", value: `${this.saconfig.IsDefault}` },
-				{ name: "Session-Id", value: Utility.getCurrentSessionId() }
-			]
-			if (additionalParameters) {
-				params.concat(additionalParameters);
+        public getTelemetryParameter(additionalParameters: Mscrm.EventParameter[]): Mscrm.EventParameter[] {
+            var params: Mscrm.EventParameter[] = [
+                { name: "SuggestionForEntityId", value: this.recordId },
+                { name: "SuggestedForEntityLogicalName", value: this.saconfig.SourceEntityName },
+                { name: "SuggestionProviderName", value: this.saconfig.SuggestionProvider },
+                { name: "SuggestionType", value: this.saconfig.SuggestionType },
+                { name: "IsEnabled", value: `${this.saconfig.IsEnabled}` },
+                { name: "IsDefault", value: `${this.saconfig.IsDefault}` },
+                { name: "Session-Id", value: Utility.getCurrentSessionId() }
+            ]
+            if (additionalParameters) {
+                params = params.concat(additionalParameters);
             }
-			return params;
-		}
+            return params;
+        }
     }
 
 	/**
 	 * All Telemetry event type.
 	 * */
-	export class TelemetryEventTypes {
-		public static componentName = "MscrmControls.SmartAssistAnyEntityControl.";
-		public static ControlInitializationStarted = TelemetryEventTypes.componentName + "ControlInitializationStarted";
-		public static DataFetchedFromAPI = TelemetryEventTypes.componentName + "DataFetchedFromAPI";
-		public static ThirdpartyBotNotEnabled = TelemetryEventTypes.componentName + "ThirdpartyBotNotEnabled";
-		public static DataFetchedFromCache = TelemetryEventTypes.componentName + "DataFetchedFromCache";
-		public static NoDataFound = TelemetryEventTypes.componentName + "NoDataFound";
-		public static NoDataFoundFromAPI = TelemetryEventTypes.componentName + "NoDataFoundFromAPI";
-		public static NoDataFoundInCache = TelemetryEventTypes.componentName + "NoDataFoundInCache";
-		public static CacheInitializationFailed = TelemetryEventTypes.componentName + "CacheInitializationFailed";
-		public static FetchingDataFromAPI = TelemetryEventTypes.componentName + "FetchingDataFromAPI";
-		public static DataFetchedFromCachePool = TelemetryEventTypes.componentName + "DataFetchedFromCachePool";
-		public static ParameterValidationFailed = TelemetryEventTypes.componentName + "ParameterValidationFailed";
-		public static ContainerStateIsDisabled = TelemetryEventTypes.componentName + "ContainerStateIsDisabled";
-		public static FailedToFetchDataFromAPI = TelemetryEventTypes.componentName + "FailedToFetchDataFromAPI";
-		public static FailedToFetchDataFromCache = TelemetryEventTypes.componentName + "FailedToFetchDataFromCache";
-		public static FailedToFetchData = TelemetryEventTypes.componentName + "FailedToFetchData";
-		public static CachePoolAddOrUpdateFailed = TelemetryEventTypes.componentName + "CachePoolAddOrUpdateFailed";
-		public static FailedToFetchDataFromCachePool = TelemetryEventTypes.componentName + "FailedToFetchDataFromCachePool";
-		public static CachePoolUpdated = TelemetryEventTypes.componentName + "CachePoolUpdated";
-		public static SuggestionProviderNotFound = TelemetryEventTypes.componentName + "SuggestionProviderNotFound";
-		public static SuggestionProviderFound = TelemetryEventTypes.componentName + "SuggestionProviderFound";
-		public static InitiatingRMControl = TelemetryEventTypes.componentName + "InitiatingRMControl";
-		public static DismissPreviousSuggestion = TelemetryEventTypes.componentName + "DismissPreviousSuggestion";
-		public static FailedToDismissPreviousSuggestion = TelemetryEventTypes.componentName + "FailedToDismissPreviousSuggestion";
-		public static ClearedCacheOnFPBEvent = TelemetryEventTypes.componentName + "ClearedCacheOnFPBEvent";
-		public static FPBEventReceived = TelemetryEventTypes.componentName + "FPBEventReceived";
-		public static FailedToRenderFPBSuggestion = TelemetryEventTypes.componentName + "FailedToRenderFPBSuggestion";
-		public static InitFailed = TelemetryEventTypes.componentName + "InitFailed";
-		public static InitStarted = TelemetryEventTypes.componentName + "InitStarted";
-		public static NoSuggestionsFoundAfterDismiss = TelemetryEventTypes.componentName + "NoSuggestionsFoundAfterDismiss";
-		public static AISuggestionsNotSupportedForNonEnglishUser = TelemetryEventTypes.componentName + "AISuggestionsNotSupportedForNonEnglishUser";
-	}
+    export class TelemetryEventTypes {
+        public static componentName = "MscrmControls.SmartAssistAnyEntityControl.";
+        public static ControlInitializationStarted = TelemetryEventTypes.componentName + "ControlInitializationStarted";
+        public static DataFetchedFromAPI = TelemetryEventTypes.componentName + "DataFetchedFromAPI";
+        public static ThirdpartyBotNotEnabled = TelemetryEventTypes.componentName + "ThirdpartyBotNotEnabled";
+        public static DataFetchedFromCache = TelemetryEventTypes.componentName + "DataFetchedFromCache";
+        public static NoDataFound = TelemetryEventTypes.componentName + "NoDataFound";
+        public static NoDataFoundFromAPI = TelemetryEventTypes.componentName + "NoDataFoundFromAPI";
+        public static NoDataFoundInCache = TelemetryEventTypes.componentName + "NoDataFoundInCache";
+        public static CacheInitializationFailed = TelemetryEventTypes.componentName + "CacheInitializationFailed";
+        public static FetchingDataFromAPI = TelemetryEventTypes.componentName + "FetchingDataFromAPI";
+        public static DataFetchedFromCachePool = TelemetryEventTypes.componentName + "DataFetchedFromCachePool";
+        public static ParameterValidationFailed = TelemetryEventTypes.componentName + "ParameterValidationFailed";
+        public static ContainerStateIsDisabled = TelemetryEventTypes.componentName + "ContainerStateIsDisabled";
+        public static FailedToFetchDataFromAPI = TelemetryEventTypes.componentName + "FailedToFetchDataFromAPI";
+        public static FailedToFetchDataFromCache = TelemetryEventTypes.componentName + "FailedToFetchDataFromCache";
+        public static FailedToFetchData = TelemetryEventTypes.componentName + "FailedToFetchData";
+        public static CachePoolAddOrUpdateFailed = TelemetryEventTypes.componentName + "CachePoolAddOrUpdateFailed";
+        public static FailedToFetchDataFromCachePool = TelemetryEventTypes.componentName + "FailedToFetchDataFromCachePool";
+        public static CachePoolUpdated = TelemetryEventTypes.componentName + "CachePoolUpdated";
+        public static SuggestionProviderNotFound = TelemetryEventTypes.componentName + "SuggestionProviderNotFound";
+        public static SuggestionProviderFound = TelemetryEventTypes.componentName + "SuggestionProviderFound";
+        public static InitiatingRMControl = TelemetryEventTypes.componentName + "InitiatingRMControl";
+        public static DismissPreviousSuggestion = TelemetryEventTypes.componentName + "DismissPreviousSuggestion";
+        public static FailedToDismissPreviousSuggestion = TelemetryEventTypes.componentName + "FailedToDismissPreviousSuggestion";
+        public static ClearedCacheOnFPBEvent = TelemetryEventTypes.componentName + "ClearedCacheOnFPBEvent";
+        public static FPBEventReceived = TelemetryEventTypes.componentName + "FPBEventReceived";
+        public static FailedToRenderFPBSuggestion = TelemetryEventTypes.componentName + "FailedToRenderFPBSuggestion";
+        public static InitFailed = TelemetryEventTypes.componentName + "InitFailed";
+        public static InitStarted = TelemetryEventTypes.componentName + "InitStarted";
+        public static NoSuggestionsFoundAfterDismiss = TelemetryEventTypes.componentName + "NoSuggestionsFoundAfterDismiss";
+        public static AISuggestionsNotSupportedForNonEnglishUser = TelemetryEventTypes.componentName + "AISuggestionsNotSupportedForNonEnglishUser";
+    }
 }
--- solutions/SmartAssist/Client/Controls/SmartassistPanelControl/Utils/TelemetryHelper.ts
+++ solutions/SmartAssist/Client/Controls/SmartassistPanelControl/Utils/TelemetryHelper.ts
@@ -1,23 +1,23 @@
 ﻿module MscrmControls.SmartassistPanelControl {
 
-	export class TelemetryHelper {
-		private recordId: string;
-		private anchorTabEntityLogicalName;
+    export class TelemetryHelper {
+        private recordId: string;
+        private anchorTabEntityLogicalName;
 
-		constructor(recordId: string, entityLogicalName: string) {
-			this.recordId = recordId;
-			this.anchorTabEntityLogicalName = entityLogicalName;
-		}
+        constructor(recordId: string, entityLogicalName: string) {
+            this.recordId = recordId;
+            this.anchorTabEntityLogicalName = entityLogicalName;
+        }
 
 		/**
 		 * Update recordid and entityLogicalName for telemetry.
 		 * @param recordId
 		 * @param entityLogicalName
 		 */
-		public updateValues(recordId: string, entityLogicalName: string) {
-			this.recordId = recordId;
-			this.anchorTabEntityLogicalName = entityLogicalName;
-		}
+        public updateValues(recordId: string, entityLogicalName: string) {
+            this.recordId = recordId;
+            this.anchorTabEntityLogicalName = entityLogicalName;
+        }
 
 		/**
 		 * Log telemetry error
@@ -25,76 +25,76 @@
 		 * @param error
 		 * @param additionalParameter
 		 */
-		public logTelemetryError(eventType: string, error: any, additionalParameter: Mscrm.EventParameter[]) {
-			const params = this.getTelemetryParameter(additionalParameter,)
-			SmartassistPanelControl._context.reporting.reportFailure(eventType, error, "TSG-TODO", params);
-		}
+        public logTelemetryError(eventType: string, error: any, additionalParameter: Mscrm.EventParameter[]) {
+            const params = this.getTelemetryParameter(additionalParameter,)
+            SmartassistPanelControl._context.reporting.reportFailure(eventType, error, "TSG-TODO", params);
+        }
 
 		/**
 		 * log success event.
 		 * @param eventType
 		 * @param additionalParameter
 		 */
-		public logTelemetrySuccess(eventType: string, additionalParameter: Mscrm.EventParameter[]) {
-			const params = this.getTelemetryParameter(additionalParameter);
-			SmartassistPanelControl._context.reporting.reportSuccess(eventType, params);
-		}
+        public logTelemetrySuccess(eventType: string, additionalParameter: Mscrm.EventParameter[]) {
+            const params = this.getTelemetryParameter(additionalParameter);
+            SmartassistPanelControl._context.reporting.reportSuccess(eventType, params);
+        }
 
 		/**
 		 * Get Telemetry event parameter
 		 * @param additionalParameters
 		 */
-		public getTelemetryParameter(additionalParameters: Mscrm.EventParameter[]): Mscrm.EventParameter[] {
-			var params: Mscrm.EventParameter[] = [
-				{ name: "SuggestionForEntityId", value: this.recordId },
-				{ name: "SuggestedForEntityLogicalName", value: this.anchorTabEntityLogicalName },
-				{ name: "Session-Id", value: Utility.getCurrentSessionId() }
-			]
-			if (additionalParameters) {
-				params.concat(additionalParameters);
-			}
-			return params;
-		}
-	}
+        public getTelemetryParameter(additionalParameters: Mscrm.EventParameter[]): Mscrm.EventParameter[] {
+            var params: Mscrm.EventParameter[] = [
+                { name: "SuggestionForEntityId", value: this.recordId },
+                { name: "SuggestedForEntityLogicalName", value: this.anchorTabEntityLogicalName },
+                { name: "Session-Id", value: Utility.getCurrentSessionId() }
+            ]
+            if (additionalParameters) {
+                params = params.concat(additionalParameters);
+            }
+            return params;
+        }
+    }
 
 	/**
 	 * All Telemetry event type.
 	 * */
-	export class TelemetryEventTypes {
-		public static componentName = "MscrmControls.SmartassistPanelControl.";
-		public static InitFailed = TelemetryEventTypes.componentName + "InitFailed";
-		public static InitCompleted = TelemetryEventTypes.componentName + "InitCompleted";
-		public static InitStarted = TelemetryEventTypes.componentName + "InitStarted"
-		public static UpdateViewStarted = TelemetryEventTypes.componentName + "UpdateViewStarted";
-		public static SessionInitStarted = TelemetryEventTypes.componentName + "SessionInitStarted";
-		public static RenderSuggestionStarted = TelemetryEventTypes.componentName + "RenderSuggestionStarted";
-		public static ConfigNotFound = TelemetryEventTypes.componentName + "ConfigNotFound";
-		public static AllConfigsDisabled = TelemetryEventTypes.componentName + "AllConfigsDisabled";
-		public static DIAPackageNotInstalled = TelemetryEventTypes.componentName + "DIAPackageNotInstalled";
-		public static CaseSettingDisabled = TelemetryEventTypes.componentName + "CaseSettingIsDisabled";
-		public static KBSettingDisabled = TelemetryEventTypes.componentName + "KBSettingIsDisabled";
-		public static ThirdPartyBotDisabled = TelemetryEventTypes.componentName + "ThirdPartyBotIsDisabled";
-		public static SAConfigFetchedFromCache = TelemetryEventTypes.componentName + "SAConfigFetchedFromCache";
-		public static AppProfileAssociationNotFound = TelemetryEventTypes.componentName + "AppProfileAssociationNotFound";
-		public static FetchingDefaultSAConfig = TelemetryEventTypes.componentName + "FetchingDefaultSAConfig";
-		public static ExceptionInFetchingSAConfigData = TelemetryEventTypes.componentName + "ExceptionInFetchingSAConfigData";
-		public static NoSAConfigFound = TelemetryEventTypes.componentName + "NoSAConfigFound";
-		public static DefaultSAConfigNotFound = TelemetryEventTypes.componentName + "DefaultSAConfigNotFound";
-		public static InvalidSourceEntityName = TelemetryEventTypes.componentName + "InvalidSourceEntityName";
-		public static ExceptionInFetchingSAConfigFromCache = TelemetryEventTypes.componentName + "ExceptionInFetchingSAConfigFromCache";
-		public static ThirdPartyBotNotFoundInWorkStream = TelemetryEventTypes.componentName + "ThirdPartyBotNotFoundInWorkStream";
-		public static LiveWorkStreamIdNotFound = TelemetryEventTypes.componentName + "LiveWorkStreamIdNotFound";
-		public static ExceptionInFetchingTPBDetails = TelemetryEventTypes.componentName + "ExceptionInFetchingTPBDetails";
-		public static FetchingSuggestionSettingsFromCache = TelemetryEventTypes.componentName + "FetchingSuggestionSettingsFromCache";
-		public static FetchingSuggestionSettingsFromAPI = TelemetryEventTypes.componentName + "FetchingSuggestionSettingsFromAPI";
-		public static SuggestionSettingsNotFound = TelemetryEventTypes.componentName + "SuggestionSettingsNotFound";
-		public static ExceptionInSuggestionSettings = TelemetryEventTypes.componentName + "ExceptionInSuggestionSettings";
-		public static SessionSwitchDetected = TelemetryEventTypes.componentName + "SessionSwitchDetected";
-		public static UnbindingOldSAAnyEntityControl = TelemetryEventTypes.componentName + "UnbindingOldSAAnyEntityControl";
-		public static SessionSwitchCECEventReceived = TelemetryEventTypes.componentName + "SessionSwitchCECEventReceived";
-		public static WebresourceLoadingFailed = TelemetryEventTypes.componentName + "WebResourceLoadingFailed";
-		public static WebresourceLoadedSuccessfully = TelemetryEventTypes.componentName + "WebresourceLoadedSuccessfully";
-		public static RenderingSmartAssistAnyEntityControl = TelemetryEventTypes.componentName + "RenderingSmartAssistAnyEntityControl";
-		public static ErrorInCheckEmptyStatus = TelemetryEventTypes.componentName + "ErrorInCheckEmptyStatus";
-	}
+    export class TelemetryEventTypes {
+        public static componentName = "MscrmControls.SmartassistPanelControl.";
+        public static InitFailed = TelemetryEventTypes.componentName + "InitFailed";
+        public static InitCompleted = TelemetryEventTypes.componentName + "InitCompleted";
+        public static InitStarted = TelemetryEventTypes.componentName + "InitStarted"
+        public static UpdateViewStarted = TelemetryEventTypes.componentName + "UpdateViewStarted";
+        public static SessionInitStarted = TelemetryEventTypes.componentName + "SessionInitStarted";
+        public static RenderSuggestionStarted = TelemetryEventTypes.componentName + "RenderSuggestionStarted";
+        public static ConfigNotFound = TelemetryEventTypes.componentName + "ConfigNotFound";
+        public static AllConfigsDisabled = TelemetryEventTypes.componentName + "AllConfigsDisabled";
+        public static DIAPackageNotInstalled = TelemetryEventTypes.componentName + "DIAPackageNotInstalled";
+        public static CaseSettingDisabled = TelemetryEventTypes.componentName + "CaseSettingIsDisabled";
+        public static KBSettingDisabled = TelemetryEventTypes.componentName + "KBSettingIsDisabled";
+        public static ThirdPartyBotDisabled = TelemetryEventTypes.componentName + "ThirdPartyBotIsDisabled";
+        public static SAConfigFetchedFromCache = TelemetryEventTypes.componentName + "SAConfigFetchedFromCache";
+        public static AppProfileAssociationNotFound = TelemetryEventTypes.componentName + "AppProfileAssociationNotFound";
+        public static FetchingDefaultSAConfig = TelemetryEventTypes.componentName + "FetchingDefaultSAConfig";
+        public static ExceptionInFetchingSAConfigData = TelemetryEventTypes.componentName + "ExceptionInFetchingSAConfigData";
+        public static NoSAConfigFound = TelemetryEventTypes.componentName + "NoSAConfigFound";
+        public static DefaultSAConfigNotFound = TelemetryEventTypes.componentName + "DefaultSAConfigNotFound";
+        public static InvalidSourceEntityName = TelemetryEventTypes.componentName + "InvalidSourceEntityName";
+        public static ExceptionInFetchingSAConfigFromCache = TelemetryEventTypes.componentName + "ExceptionInFetchingSAConfigFromCache";
+        public static ThirdPartyBotNotFoundInWorkStream = TelemetryEventTypes.componentName + "ThirdPartyBotNotFoundInWorkStream";
+        public static LiveWorkStreamIdNotFound = TelemetryEventTypes.componentName + "LiveWorkStreamIdNotFound";
+        public static ExceptionInFetchingTPBDetails = TelemetryEventTypes.componentName + "ExceptionInFetchingTPBDetails";
+        public static FetchingSuggestionSettingsFromCache = TelemetryEventTypes.componentName + "FetchingSuggestionSettingsFromCache";
+        public static FetchingSuggestionSettingsFromAPI = TelemetryEventTypes.componentName + "FetchingSuggestionSettingsFromAPI";
+        public static SuggestionSettingsNotFound = TelemetryEventTypes.componentName + "SuggestionSettingsNotFound";
+        public static ExceptionInSuggestionSettings = TelemetryEventTypes.componentName + "ExceptionInSuggestionSettings";
+        public static SessionSwitchDetected = TelemetryEventTypes.componentName + "SessionSwitchDetected";
+        public static UnbindingOldSAAnyEntityControl = TelemetryEventTypes.componentName + "UnbindingOldSAAnyEntityControl";
+        public static SessionSwitchCECEventReceived = TelemetryEventTypes.componentName + "SessionSwitchCECEventReceived";
+        public static WebresourceLoadingFailed = TelemetryEventTypes.componentName + "WebResourceLoadingFailed";
+        public static WebresourceLoadedSuccessfully = TelemetryEventTypes.componentName + "WebresourceLoadedSuccessfully";
+        public static RenderingSmartAssistAnyEntityControl = TelemetryEventTypes.componentName + "RenderingSmartAssistAnyEntityControl";
+        public static ErrorInCheckEmptyStatus = TelemetryEventTypes.componentName + "ErrorInCheckEmptyStatus";
+    }
 }
