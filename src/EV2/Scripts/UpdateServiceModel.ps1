3d67b707c3cda5571ce77661b36c7a82a687c6cc
Rashi Goyal <rashigoy@microsoft.com>
2022-01-05 16:01:50 +0000

    Merged PR 678683: Pull Request 678681: Fix auth for blob copy
    _< Thank you for your contribution to Productivity Tools repository. >_
    
    ### Description
    _EV2 changes for deployment of blob copy_
    
    ### Unit/Integration tests
    _< If no tests, append justification for why and associated bugs/tasks tracking the test gaps. >_
    
    ### Test org
    _< Please provide the link to a test org (e.g. [Aurora/Online](https://portal.microsoftaurora365.com/)) with **managed solutions imported** where others can test your changes. >_
    
    ### Visualization
    _< Please provide screenshots / screen recordings of your changes, as appropriate. >_
    
    ### Before submitting this PR, please make sure:
    - [ ] Run a local build
    - [ ] Tested with patch solution
    - [ ] Choose squash commit when completing the PR
    
    Deployed changes to a test environment created in GCC Prod subscription:
    https://dev.azure.com/dynamicscrm/OneCRM/_releaseProgress?releaseId=2458533&environmentId=7494418&_a=release-environment-logs
    
    Problem: Authentication was  failing for GCC Prod Environment
    Fix: Change auth mechanism based on the permissions
    
    Related work items: #2546436

--- src/EV2/Scripts/UpdateServiceModel.ps1
+++ src/EV2/Scripts/UpdateServiceModel.ps1
@@ -7,16 +7,49 @@ param(
     $SubscriptionId = "7da41661-fb51-45ba-b8f2-b4c136cb904b",
 
     [Parameter(Mandatory = $False)]
-    $ToEmail = "OcEV2CoreTeam@microsoft.com"
+    $ToEmail = "OcEV2CoreTeam@microsoft.com",
+
+    [Parameter(Mandatory = $False)]
+    $Geo = "United States",
+
+    [Parameter(Mandatory = $False)]
+    $Cloud = "octest",
+
+    [Parameter(Mandatory = $False)]
+    $RolloutType = "Major",
+
+    [Parameter(Mandatory = $False)]
+    $TenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47",
+
+    [Parameter(Mandatory = $False)]
+    $GCC = "Public",
+
+    [Parameter(Mandatory = $False)]
+    $adminKVSubId = "7da41661-fb51-45ba-b8f2-b4c136cb904b",
+
+    [Parameter(Mandatory = $False)]
+    $BlobFolder = "drop/outputs/build/TedCBuildArtifact",
+
+    [Parameter(Mandatory = $False)]
+    $CurrPath = ""
 )
 
-$CurrPath = "$(Get-Location)\Src\EV2"
+if([string]::IsNullOrEmpty($CurrPath)){
+	$CurrPath = "$(Get-Location)\WebChatControl\EV2"
+}
 
 function  OpenFileAndUpdateContents([string]$ConfigFile) {
 Write-Host $ConfigFile
 $contents = Get-Content $ConfigFile | Out-String
 
 $contents = $contents -replace "\(CloudType\)",$CloudType
+$contents = $contents -replace "\(Cloud\)",$Cloud
+$contents = $contents -replace "\(Geo\)",$Geo
+$contents = $contents -replace "\(TenantId\)",$TenantId
+$contents = $contents -replace "\(adminKVSubId\)",$adminKVSubId
+$contents = $contents -replace "\(BlobFolder\)",$BlobFolder
+$contents = $contents -replace "\(GCC\)",$GCC
+$contents = $contents -replace "\(Cloud\)",$Cloud
 $contents = $contents -replace "\(SubscriptionId\)",$SubscriptionId
 $contents = $contents -replace "\(imageName\)","adm-ubuntu-1804-l"
 $contents = $contents -replace "\(imageVersion\)","v17"
@@ -29,7 +62,10 @@ Set-Content $ConfigFile $contents
 $ConfigFile =  "$CurrPath\BlobUpload\ServiceModel.json"
 OpenFileAndUpdateContents -ConfigFile $ConfigFile;
 
-
 #Update contents for rollouts
 $ConfigFile =  "$CurrPath\BlobUpload\FlowControlRolloutSpec.json"
+OpenFileAndUpdateContents -ConfigFile $ConfigFile;
+
+#Update contents for scope binding
+$ConfigFile =  "$CurrPath\BlobUpload\ScopeBindings.json"
 OpenFileAndUpdateContents -ConfigFile $ConfigFile;
