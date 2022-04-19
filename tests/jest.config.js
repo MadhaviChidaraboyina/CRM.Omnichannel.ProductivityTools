d8478b2cedd4cc1f0a45595a6340844a6e19989e
Akhil Agarwal (HCL America Inc) <v-akagar@microsoft.com>
2022-04-19 21:07:40 +0000

    Merged PR 721671: Add global configuration for code coverage
    _< Thank you for your contribution to Productivity Tools repository. >_
    
    ### Description
    Add global configuration for code coverage
    
    ### Unit/Integration tests
    Add global configuration for code coverage
    
    ![image.png](https://dynamicscrm.visualstudio.com/b276c3e1-2902-46bd-a686-484157b97f48/_apis/git/repositories/65512151-aff7-4b31-9d3f-909da7920dc9/pullRequests/721671/attachments/image.png)
    
    ### Test org
    _< Please provide the link to a test org (e.g. [Aurora/Online](https://portal.microsoftaurora365.com/)) with **managed solutions imported** where others can test your changes. >_
    
    ### Visualization
    _< Please provide screenshots / screen recordings of your changes, as appropriate. >_
    
    ### Before submitting this PR, please make sure:
    - [ ] Run a local build
    - [ ] Tested with patch solution
    - [ ] Choose squash commit when completing the PR
    
    Add global configuration for code coverage
    
    Related work items: #2678623

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,16 +1,24 @@
 module.exports = {
-  "roots": ["."],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
+  roots: ["."],
+  testMatch: [
+    "**/tests/**/?(*.)+(spec|test).+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
-  "transform": {
+  transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
   // The directory where Jest should output its coverage files
   coverageDirectory: "../drop/coverage",
   coverageReporters: ["text", "cobertura", "lcov", "json-summary"],
-
+  collectCoverage: true,
+  coverageThreshold: {
+      global: {
+          branches: 0,
+          functions: 0,
+          lines: 0,
+          statements: 0
+      }
+  },
   reporters: [
     "default",
     [
